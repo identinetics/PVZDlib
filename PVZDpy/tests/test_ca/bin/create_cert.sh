e8c958c3a5581408fcda708bb603aec5cdc139fa
rhoerbe <rainer@hoerbe.at>
2018-10-30 23:57:29 +0100

    add cert for extranet.unido.org

--- PVZDpy/tests/test_ca/bin/create_cert.sh
+++ PVZDpy/tests/test_ca/bin/create_cert.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# self
 openssl req -new \
     -subj /CN=idp.identinetics.com \
     -config etc/openssl.cnf -batch -days 7500 \
@@ -7,13 +8,14 @@ openssl req -new \
     -keyout certs/key.pem -nodes -newkey rsa:2048 \
     -passout pass:changeit
 
-
 openssl ca \
     -config etc/openssl.cnf -batch -days 3650 \
     -in certs/cert.csr \
     -keyfile ca/root-key.pem \
     -key changeit
 
+
+# idp.example.com
 openssl req -new \
     -subj /CN=idp.example.com \
     -config etc/openssl.cnf -batch -days 7500 \
@@ -21,13 +23,14 @@ openssl req -new \
     -keyout certs/idp.example.com_key.pem -nodes -newkey rsa:2048 \
     -passout pass:changeit
 
-
 openssl ca \
     -config etc/openssl.cnf -batch -days 3650 \
     -in certs/idp.example.com.csr \
     -keyfile ca/root-key.pem \
     -key changeit
 
+
+# gondor.wien.gv.at
 openssl req -new \
     -subj /CN=gondor.wien.gv.at \
     -config etc/openssl.cnf -batch -days 7500 \
@@ -35,10 +38,26 @@ openssl req -new \
     -keyout certs/gondor.wien.gv.at_key.pem -nodes -newkey rsa:2048 \
     -passout pass:changeit
 
-
 openssl ca \
     -config etc/openssl.cnf -batch -days 3650 \
     -in certs/gondor.wien.gv.at_csr.pem \
     -out certs/gondor.wien.gv.at_crt.pem \
+    -keyfile ca/root-key.pem \
+    -key changeit
+
+
+# extranet.unido.org
+fqdn=extranet.unido.org
+openssl req -new \
+    -subj /CN=${fqdn} \
+    -config etc/openssl.cnf -batch -days 7500 \
+    -out certs/${fqdn}_csr.pem  -text \
+    -keyout certs/${fqdn}_key.pem -nodes -newkey rsa:2048 \
+    -passout pass:changeit
+
+openssl ca \
+    -config etc/openssl.cnf -batch -days 3650 \
+    -in certs/${fqdn}_csr.pem \
+    -out certs/${fqdn}_crt.pem \
     -keyfile ca/root-key.pem \
     -key changeit
