9c0690b175897dd9cdad4b78064fd4120608f18a
rhoerbe <rainer@hoerbe.at>
2018-10-30 09:06:58 +0100

    refactored are_dir_trees_equal() into separate module for reuse

--- PVZDpy/tests/test_gitHandler.py
+++ PVZDpy/tests/test_gitHandler.py
@@ -4,34 +4,10 @@ import pytest
 import shutil
 import tempfile
 from PVZDpy.githandler import GitHandler
+from PVZDpy.utilities import are_dir_trees_equal, opj
 
 #path_prefix = 'PVZDpy/tests/testdata/githandler/'
 path_prefix = 'testdata/githandler/'
-
-def are_dir_trees_equal(dir1, dir2) -> bool:
-    """ Compare two directories recursively. Files in each directory are
-        assumed to be equal if their names and contents are equal.
-        @return: True if the directory trees are the same and  there were no errors
-            while accessing the directories or files, False otherwise.
-        Kudos to Mateusz Kobos: https://stackoverflow.com/questions/4187564/
-    """
-    dirs_cmp = filecmp.dircmp(dir1, dir2)
-    if len(dirs_cmp.left_only)>0 or len(dirs_cmp.right_only)>0 or \
-        len(dirs_cmp.funny_files)>0:
-        return False
-    (_, mismatch, errors) =  filecmp.cmpfiles(
-        dir1, dir2, dirs_cmp.common_files, shallow=False)
-    if len(mismatch)>0 or len(errors)>0:
-        return False
-    for common_dir in dirs_cmp.common_dirs:
-        new_dir1 = os.path.join(dir1, common_dir)
-        new_dir2 = os.path.join(dir2, common_dir)
-        if not are_dir_trees_equal(new_dir1, new_dir2):
-            return False
-    return True
-
-def opj(*args):
-    return os.path.join(args[0], args[1])
 
 @pytest.fixture
 def githandler(tmpdir):
---
+++ PVZDpy/utilities.py
@@ -0,0 +1,28 @@
+import filecmp
+import os
+
+def are_dir_trees_equal(dir1, dir2) -> bool:
+    """ Compare two directories recursively. Files in each directory are
+        assumed to be equal if their names and contents are equal.
+        @return: True if the directory trees are the same and  there were no errors
+            while accessing the directories or files, False otherwise.
+        Kudos to Mateusz Kobos: https://stackoverflow.com/questions/4187564/
+    """
+    dirs_cmp = filecmp.dircmp(dir1, dir2)
+    if len(dirs_cmp.left_only)>0 or len(dirs_cmp.right_only)>0 or \
+        len(dirs_cmp.funny_files)>0:
+        return False
+    (_, mismatch, errors) =  filecmp.cmpfiles(
+        dir1, dir2, dirs_cmp.common_files, shallow=False)
+    if len(mismatch)>0 or len(errors)>0:
+        return False
+    for common_dir in dirs_cmp.common_dirs:
+        new_dir1 = os.path.join(dir1, common_dir)
+        new_dir2 = os.path.join(dir2, common_dir)
+        if not are_dir_trees_equal(new_dir1, new_dir2):
+            return False
+    return True
+
+def opj(left, right):
+    return os.path.join(left, right)
+
