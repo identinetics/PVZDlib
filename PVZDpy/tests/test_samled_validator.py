d26780650e3cf0679964c1d95ab7103c32cb9291
rhoerbe <rainer@hoerbe.at>
2018-11-07 22:02:51 +0100

    reactivate test01-group; add assert

--- PVZDpy/tests/test_samled_validator.py
+++ PVZDpy/tests/test_samled_validator.py
@@ -25,47 +25,53 @@ def run_test_with_edpath(file_index: int, sigval=False, test_index=None):
     ed = SamlEdValidator(poldir1())
     ed.validate_entitydescriptor(ed_path_new=ed_path_testin(file_index), sigval=sigval)
     ed_dict = ed.get_obj_as_dict()
-    with open(ed_path_testout(file_index, test_index), 'w') as fd:
-        fd.write(json.dumps(ed_dict, indent=2, sort_keys=True))
+    fn1_testout = ed_path_testout(file_index, test_index)
+    fn2_testexp = ed_path_test_expected(file_index, test_index)
+    with open(fn1_testout, 'w') as fd1:
+        fd1.write(json.dumps(ed_dict, indent=2, sort_keys=True))
+    with open(fn1_testout) as fd1:
+        with open(fn2_testexp) as fd2:
+            assert fd1.read() == fd2.read()
+    os.unlink(fn1_testout)
 
-# def test01_edval_edpath1():
-#     run_test_with_edpath(1, sigval=False, test_index=1)
-#
-# def test01_edval_edpath2():
-#     run_test_with_edpath(2, sigval=False, test_index=1)
-#
-# def test01_edval_edpath3():
-#     run_test_with_edpath(3, sigval=False, test_index=1)
-#
-# def test01_edval_edpath4():
-#     run_test_with_edpath(4, sigval=False, test_index=1)
-#
-# def test01_edval_edpath5():
-#     run_test_with_edpath(5, sigval=False, test_index=1)
-#
-# def test01_edval_edpath6():
-#     run_test_with_edpath(6, sigval=False, test_index=1)
-#
-# def test01_edval_edpath7():
-#     run_test_with_edpath(7, sigval=False, test_index=1)
-#
-# def test01_edval_edpath8():
-#     run_test_with_edpath(8, sigval=True, test_index=1)
-#
-# def test01_edval_edpath9():
-#     run_test_with_edpath(9, sigval=True, test_index=1)
-#
-# def test01_edval_edpath10():
-#     run_test_with_edpath(10, sigval=True, test_index=1)
-#
-# def test01_edval_edpath11():
-#     run_test_with_edpath(11, sigval=True, test_index=1)
-#
-# def test01_edval_edpath12():
-#     run_test_with_edpath(12, sigval=True, test_index=1)
-#
-# def test01_edval_edpath13():
-#     run_test_with_edpath(13, sigval=True, test_index=1)
+def test01_edval_edpath1():
+    run_test_with_edpath(1, sigval=False, test_index=1)
+
+def test01_edval_edpath2():
+    run_test_with_edpath(2, sigval=False, test_index=1)
+
+def test01_edval_edpath3():
+    run_test_with_edpath(3, sigval=False, test_index=1)
+
+def test01_edval_edpath4():
+    run_test_with_edpath(4, sigval=False, test_index=1)
+
+def test01_edval_edpath5():
+    run_test_with_edpath(5, sigval=False, test_index=1)
+
+def test01_edval_edpath6():
+    run_test_with_edpath(6, sigval=False, test_index=1)
+
+def test01_edval_edpath7():
+    run_test_with_edpath(7, sigval=False, test_index=1)
+
+def test01_edval_edpath8():
+    run_test_with_edpath(8, sigval=True, test_index=1)
+
+def test01_edval_edpath9():
+    run_test_with_edpath(9, sigval=True, test_index=1)
+
+def test01_edval_edpath10():
+    run_test_with_edpath(10, sigval=True, test_index=1)
+
+def test01_edval_edpath11():
+    run_test_with_edpath(11, sigval=True, test_index=1)
+
+def test01_edval_edpath12():
+    run_test_with_edpath(12, sigval=True, test_index=1)
+
+def test01_edval_edpath13():
+    run_test_with_edpath(13, sigval=True, test_index=1)
 
 def run_test_with_xmlstr(file_index: int, sigval=False, test_index=None):
     ed = SamlEdValidator(poldir1())
@@ -79,7 +85,7 @@ def run_test_with_xmlstr(file_index: int, sigval=False, test_index=None):
     with open(fn1_testout) as fd1:
         with open(fn2_testexp) as fd2:
             assert fd1.read() == fd2.read()
-    #os.unlink(fn1_testout)
+    os.unlink(fn1_testout)
 
 def test02_edval_str01():
     run_test_with_xmlstr(1, sigval=False, test_index=2)
