9d63efc0ffd99639f21f04f93bba85d140238748
rhoerbe <rainer@hoerbe.at>
2019-01-30 23:02:41 +0100

    fix classpass using external (old) project

--- PVZDpy/docs/runtime_env_dev.sh
+++ PVZDpy/docs/runtime_env_dev.sh
@@ -1,3 +1,3 @@
-export CLASSPATH=/Users/admin/devl/python/identinetics/PVZDpolman/PVZDlib/MOA-SPSS/moa-sig-lib-latest.jar:/Users/admin/devl/python/identinetics/PVZDpolman/PVZDlib/MOA-SPSS/lib/*:/Users/admin/devl/python/identinetics/PVZDpolman/PVZDlib/unittests/junit-4.11.jar:/Users/admin/devl/python/identinetics/PVZDpolman/PVZDlib/PVZDjava/*
+export CLASSPATH=/Users/admin/devl/python/identinetics/PVZDlib/MOA-SPSS/moa-sig-lib-latest.jar:/Users/admin/devl/python/identinetics/PVZDlib/MOA-SPSS/lib/*:/Users/admin/devl/python/identinetics/PVZDlib/unittests/junit-4.11.jar:/Users/admin/devl/python/identinetics/PVZDlib/PVZDjava/*
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0/Contents/Home
 export DYLD_LIBRARY_PATH=/Library/Java/JavaVirtualMachines/jdk1.8.0/Contents/Home/jre/lib/server
