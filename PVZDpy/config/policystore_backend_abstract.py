936f0749a9236e62c033ad8f958eb4a229c21032
rhoerbe <rainer@hoerbe.at>
2019-02-18 21:25:25 +0100

    correct signatures of set_* in policystore_backend_abstract

--- PVZDpy/config/policystore_backend_abstract.py
+++ PVZDpy/config/policystore_backend_abstract.py
@@ -4,6 +4,8 @@ from PVZDpy.userexceptions import PolicyJournalNotInitialized
 
 class PolicyStoreBackendAbstract():
     # concrete classes must not raise PolicyJournalNotInitialized during __init__()
+    def __init__(self):
+        raise NotImplementedError()
 
     def get_policy_journal(self) -> bytes:
         try:
@@ -12,6 +14,7 @@ class PolicyStoreBackendAbstract():
             raise PolicyJournalNotInitialized
 
     def get_policy_journal_path(self) -> Path:
+        # if persistence is not in the filesystem (-> database), provide a temp copy in fs for signature verification
         raise NotImplementedError()
 
     def get_policy_journal_json(self) -> str:
@@ -32,24 +35,26 @@ class PolicyStoreBackendAbstract():
     def get_shibacl(self) -> bytes:
         raise NotImplementedError()
 
-    def set_policy_journal_xml(self):
+    def set_policy_journal_xml(self, xml_bytes: bytes):
         raise NotImplementedError()
 
-    def set_policy_journal_json(self):
+    def set_policy_journal_json(self, json_str: str):
         raise NotImplementedError()
 
-    def set_poldict_json(self):
+    def set_poldict_json(self, json_str: str):
         raise NotImplementedError()
 
-    def set_poldict_html(self):
+    def set_poldict_html(self, html_str: str):
         raise NotImplementedError()
 
-    def set_trustedcerts_report(self):
+    def set_shibacl(self, xml_bytes: bytes):
         raise NotImplementedError()
 
-    def set_shibacl(self):
+    def set_trustedcerts_report(self, t: str):
         raise NotImplementedError()
 
     def reset_policy_and_derived(self):
         # delete data except for trustedcerts_copy
         raise NotImplementedError()
+
+
