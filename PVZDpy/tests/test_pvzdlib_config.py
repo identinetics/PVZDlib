0023e23ae65f08c7788bce7ff9f7b650cb3c165c
rhoerbe <rainer@hoerbe.at>
2019-02-19 21:51:33 +0100

    fix speaker data

--- PVZDpy/tests/test_pvzdlib_config.py
+++ PVZDpy/tests/test_pvzdlib_config.py
@@ -6,21 +6,19 @@ from PVZDpy.config.appconfig_abstract import PVZDlibConfigAbstract
 from PVZDpy.trustedcerts import TrustedCerts
 from PVZDpy.userexceptions import PolicyJournalNotInitialized
 
+
 @pytest.fixture
 def testdata_dir() -> Path:
     return Path('testdata/pvzdlib_config')
+
 
 @pytest.fixture
 def testout_dir() -> Path:
     dir = Path('testout/pvzdlib_config')
     dir.mkdir(parents=True, exist_ok=True)
 
-@pytest.fixture
-def expected_poldict_json02(testdata_dir):
-    os.environ['PVZDLIB_CONFIG_MODULE'] = str(testdata_dir / 'pvzdlib_config02.py')
-    p = testdata_dir/ 'expected_results' / 'policy_journal02.json'
-    return json.load(p.open())
 
+# --- 01 ---
 
 def test_01_default_not_init():
     pvzdconf = PVZDlibConfigAbstract.get_config()
@@ -29,8 +27,42 @@ def test_01_default_not_init():
     with pytest.raises(PolicyJournalNotInitialized) as context:
         _ = backend.get_policy_journal_json()
 
-def test_02_init(expected_poldict_json02):
+
+# --- 02 ---
+
+@pytest.fixture
+def pvzdconfig02(testdata_dir):
+    os.environ['PVZDLIB_CONFIG_MODULE'] = str(testdata_dir / 'pvzdlib_config02.py')
+
+
+@pytest.fixture
+def expected_poldict_json02(testdata_dir):
+    p = testdata_dir/ 'expected_results' / 'policy_journal02.json'
+    return json.load(p.open())
+
+
+def test_02_read_existing(pvzdconfig02, expected_poldict_json02):
     pvzdconf = PVZDlibConfigAbstract.get_config()
     backend = pvzdconf.polstore_backend
     policy_journal_json = backend.get_policy_journal_json()
     assert json.loads(policy_journal_json) == expected_poldict_json02
+
+
+# --- 03 ---
+
+@pytest.fixture
+def pvzdconfig03(testdata_dir):
+    os.environ['PVZDLIB_CONFIG_MODULE'] = str(testdata_dir / 'pvzdlib_config03.py')
+
+
+@pytest.fixture
+def expected_poldict_json03(testdata_dir):
+    p = testdata_dir / 'expected_results' / 'policy_journal03.json'
+    return json.load(p.open())
+
+
+def test_03_initialize(pvzdconfig03, expected_poldict_json03):
+    pvzdconf = PVZDlibConfigAbstract.get_config()
+    backend = pvzdconf.polstore_backend
+    policy_journal_json = backend.get_policy_journal_json()
+    assert json.loads(policy_journal_json) == expected_poldict_json03
